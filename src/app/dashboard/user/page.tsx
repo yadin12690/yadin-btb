31f1f7a21a6848bd5cd9257584efc921c0787de0
Yadin Yafe <yadin258@yahoo.com>
2024-05-11 19:35:40 +0300

    display user data

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -4,24 +4,34 @@ import { useData } from "@/app/api/useData";
 import { useAuth } from "@/app/context/AuthContext";
 import { CharacterResponse } from "@/app/utils/providers/types/character";
 import { LocationResponse } from "@/app/utils/providers/types/location";
+import { useEffect } from "react";
+
+//user page
 
 export default function IndexPage() {
     const { user } = useAuth(); // Get the current user
     const { data: items, isLoading, isError } = useData(user?.role); // Fetch data based on the user's role
 
+
+    useEffect(() => {
+        console.log(items);
+
+    }, [items]);
+
     if (isLoading) return <div>Loading...</div>;
     if (isError) return <div>Error loading items</div>;
 
+
+
     return (
         <div>
-            <h1>Rick and Morty Data</h1>
+            <h1>User Rick and morty data</h1>
             <div>
                 <ul>
-                    {items && items.map(({ results }: CharacterResponse | LocationResponse, idx: number) => (
-                        <li key={results[idx].id}>{results[idx].name}</li>
-                    ))}
+                    {items && ('results' in items && (items as unknown as CharacterResponse).results.map((item, idx) => (
+                        <li key={idx}>{item.name}</li>
+                    )))}
                 </ul>
-                <h3>User Page</h3>
             </div>
         </div>
     );
